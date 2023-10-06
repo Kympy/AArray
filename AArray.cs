9be7ae1ddbb7b834deb8b07ecad81b190fdddfaf
Parrot_Yong <65384983+Kympy@users.noreply.github.com>
2023-10-06 12:30:47 +0900

    Initial commit

---
+++ AArray.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazingArray
+{
+	public class AArray<T>
+	{
+		private T[]? internalArray;
+		private int arraySize;
+		public int InternalSize
+		{
+			get { return arraySize; }
+		}
+		public int InternalMaxIndex
+		{
+			get 
+			{
+				if (arraySize == 0) return 0;
+				return arraySize - 1; 
+			}
+		}
+		private int arrayIndexPointer = 0;
+		public AArray()
+		{
+			// Default size is 10.
+			arraySize = 10;
+			internalArray = new T[arraySize];
+		}
+		/// <summary>
+		/// Make new AArray that copied value from original array.
+		/// </summary>
+		/// <param name="originArray">Original array you want to copy</param>
+		public AArray(T[] originArray)
+		{
+			CopyFullArray(originArray);
+		}
+		// Make [] property.
+		public T this[int index]
+		{
+			get
+			{
+				if (internalArray == null || index > InternalMaxIndex)
+				{
+					throw new IndexOutOfRangeException();
+				}
+				return internalArray[index];
+			}
+			set
+			{
+				if (internalArray == null || index > InternalMaxIndex)
+				{
+					throw new IndexOutOfRangeException();
+				}
+				internalArray[index] = value;
+			}
+		}
+		public virtual void CopyFullArray(T[] originArray)
+		{
+			if (originArray == null)
+			{
+				throw new ArgumentNullException();
+			}
+			arraySize = originArray.Length;
+			internalArray = new T[arraySize];
+			int index = 0;
+			foreach (var element in originArray)
+			{
+				internalArray[index] = element;
+				index++;
+			}
+		}
+		public static void CopyFullArray(T[] from, T[] to)
+		{
+			if (from == null || to == null)
+			{
+				throw new NullReferenceException();
+			}
+			for(int i = 0; i < from.Length; i++)
+			{
+				if (i >= to.Length)
+				{
+					break;
+				}
+				to[i] = from[i];
+			}
+		}
+		protected int elementCount = 0;
+		public int Count
+		{
+			get { return elementCount; }
+		}
+		public virtual void Add(T item)
+		{
+			if (internalArray == null)
+			{
+				throw new NullReferenceException();
+			}
+			if (IsNeedToResize == true)
+			{
+				Resize();
+			}
+			internalArray[arrayIndexPointer++] = item;
+			elementCount++;
+		}
+		protected bool IsNeedToResize
+		{
+			get
+			{
+				if (internalArray == null)
+				{
+					throw new NullReferenceException();
+				}
+				if (arrayIndexPointer > InternalMaxIndex)
+				{
+					return true;
+				}
+				return false;
+			}
+		}
+		protected virtual void Resize()
+		{
+			if (internalArray == null)
+			{
+				throw new NullReferenceException();
+			}
+			T[] tempArray = new T[arraySize + arraySize];
+			CopyFullArray(internalArray, tempArray);
+			arraySize = tempArray.Length;
+			internalArray = tempArray;
+		}
+		/// <summary>
+		/// Add item to AArray where item is not contained in internal array.
+		/// </summary>
+		/// <param name="item"></param>
+		public virtual void AddUnique(T item)
+		{
+			if (internalArray == null)
+			{
+				throw new NullReferenceException();
+			}
+			if (Contains(item) == true)
+			{
+				return;
+			}
+            if (IsNeedToResize == true)
+            {
+				Resize();
+            }
+			internalArray[arrayIndexPointer++] = item;
+			elementCount++;
+        }
+		public bool Contains(T item)
+		{
+			if (internalArray == null)
+			{
+				throw new NullReferenceException();
+			}
+            if (item == null)
+            {
+				throw new ArgumentNullException();
+            }
+			return Array.IndexOf<T>(internalArray, item) != -1;
+        }
+		public virtual void InsertAtIndex(T item, int targetIndex)
+		{
+
+		}
+		public virtual void AddFirst(T item)
+		{
+
+		}
+		public virtual void RemoveElement(T targetElement)
+		{
+
+		}
+		public virtual void RemoveAtIndex(int targetIndex)
+		{
+
+		}
+		public virtual void RemoveFirst()
+		{
+
+		}
+		public virtual void RemoveEnd()
+		{
+
+		}
+	}
+}
+
---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿using AmazingArray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AArray
+{
+	internal class MainFunction
+	{
+		public static void Main()
+		{
+			AddTitle("Create Int Array");
+			AArray<int> testIntArray = new AArray<int>();
+			AddTitle("Add 0~19 int value");
+			for(int i = 0; i < 12; i++)
+			{
+				testIntArray.Add(i);
+				Console.WriteLine($"Index{(i)} / Value{(testIntArray[i])} / Size{(testIntArray.InternalSize)}");
+			}
+			AddTitle("Counting Int Array");
+			Console.WriteLine($"Element Count : {testIntArray.Count}");
+			AddTitle("Contains Function Test");
+			string containResult = testIntArray.Contains(5) ? "Yes" : "No";
+			Console.WriteLine($"Is Contain '5' ? => {containResult}");
+		}
+		private static void AddTitle(string title)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("___________________________________");
+			Console.WriteLine($" {title}");
+			Console.WriteLine("===================================");
+			Console.WriteLine("");
+		}
+	}
+}
